f954fcb58b02cf0df5f0e854698e51df70c33929
Garrett Stewart <garrettbstewart@gmail.com>
2023-08-11 08:45:34 -0400

    Added js, transition to btn

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// BUTTON STUFF
+
+let getHintButton = document.querySelector(".button");
+
+getHintButton.addEventListener("click", getHint);
+
+function getHint() {
+  console.log("This Button Works");
+}
