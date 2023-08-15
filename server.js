98adf2a4cf8c33646b213195f626390e487fda59
Garrett Stewart <garrettbstewart@gmail.com>
2023-08-15 13:44:46 -0400

    Fixed to Add CSS File

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
